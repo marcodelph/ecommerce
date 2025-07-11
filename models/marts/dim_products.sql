956fabceb308211b1c42d636e520cbd88dff537f
Marco Filipe <marcoding24@gmail.com>
2025-07-11 22:40:51 +0000

    refactor: adição de número de vezes que o produto foi vendido e a receita total gerada por ele na tabela products

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,8 +1,19 @@
+-- models/marts/dim_products.sql
+
 {{
     config(
         materialized='table'
     )
 }}
+
+with product_sales as (
+    select
+        product_id,
+        count(order_id) as number_of_orders,
+        sum(item_price) as total_revenue
+    from {{ ref('stg_order_items') }}
+    group by 1
+)
 
 select
     p.product_id,
@@ -11,8 +22,12 @@ select
     p.product_weight_g,
     p.product_length_cm,
     p.product_height_cm,
-    p.product_width_cm
+    p.product_width_cm,
+    coalesce(ps.number_of_orders, 0) as number_of_orders,
+    coalesce(ps.total_revenue, 0) as total_revenue
 from
     {{ ref('stg_products') }} p
 left join {{ ref('stg_product_category_name_translation') }} t on
     p.product_category_name = t.product_category_name
+left join product_sales ps on
+    p.product_id = ps.product_id
